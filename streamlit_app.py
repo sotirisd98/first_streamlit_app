8bd3088f2dbf2c2949722f2d94e745c164c77fe8
Sotiris Dimitriou <122465683+sotirisd98@users.noreply.github.com>
2023-01-11 17:38:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('MY PARENTS NEW HEALTHY DINER')
+streamlit.header('BREAKFAST MENU)
+                 
+streamlit.text('Omega 3 & Blueberry Oat meal)
+streamlit.text('Omega 3 & Blueberry Oat meal)
+streamlit.text('Omega 3 & Blueberry Oat meal)
